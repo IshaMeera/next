5e9e6210bfc093ae88ca0449588a6e2aca02cb65
Isha <ishmeera27@gmail.com>
2023-07-14 21:51:12 +0530

    first commit

---
+++ pages/posts/first-post.js
@@ -0,0 +1,13 @@
+import Link from 'next/link';
+
+export default function FirstPost(){
+    return ( 
+    <>
+    <h1>Good morning</h1>
+    <h2> 
+        <Link href="/">Hello Milo</Link>
+    </h2>
+    </>
+    );
+
+}
